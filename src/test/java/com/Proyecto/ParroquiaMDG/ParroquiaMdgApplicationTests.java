c272ffeafd5233dba940cce787f078d9d9d83a34
Estefania <estefanianeira1@gmail.com>
2021-06-03 15:45:51 -0500

    Proyecto en Spring

---
+++ src/test/java/com/Proyecto/ParroquiaMDG/ParroquiaMdgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto.ParroquiaMDG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParroquiaMdgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
